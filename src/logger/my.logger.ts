8ede2cc45fa8cfaaa542e9729b6d9aeb7b7e5674
duong.ha <duong.ha@viaccess-orca.com>
2026-05-25 21:49:03 +0300

    Format and Colorize logging

--- src/logger/my.logger.ts
+++ src/logger/my.logger.ts
@@ -1,7 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable */
 // @ts-nocheck
 import { LoggerService } from '@nestjs/common';
 import { createLogger, format, Logger, transports } from 'winston';
+import chalk from 'chalk';
+import dayjs from 'dayjs';
 
 export class MyLogger implements LoggerService {
     private logger: Logger;
@@ -13,31 +15,48 @@ export class MyLogger implements LoggerService {
                 format.timestamp(),
                 format.simple(),
             ),
-            transports: [new transports.Console()],
+            transports: [
+                new transports.Console({
+                    format: format.combine(
+                        format.colorize(),
+                        format.printf((info) => {
+                            const strApp = chalk.green('[Nest]');
+                            const strContext = chalk.yellow(`[${String(info['context'] ?? '')}]`);
+                            return `${strApp} - ${String(info['time'])} - ${info.level} ${strContext}: ${String(info.message)}`;
+                        }),
+                    ),
+                }),
+            ],
         });
     }
 
-    log(message: string, ...args: any[]) {
-        this.logger.info(`${message} | ${args.join(' | ')}`);
+    log(message: string, context: string) {
+        const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+        this.logger.info(message, { context, time });
     }
 
-    error(message: string, ...args: any[]) {
-        this.logger.error(`${message} | ${args.join(' | ')}`);
+    error(message: string, context: string) {
+        const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+        this.logger.error(message, { context, time });
     }
 
-    warn(message: string, ...args: any[]) {
-        this.logger.warn(`${message} | ${args.join(' | ')}`);
+    warn(message: string, context: string) {
+        const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+        this.logger.warn(message, { context, time });
     }
 
-    debug(message: string, ...args: any[]) {
-        this.logger.debug(`${message} | ${args.join(' | ')}`);
+    debug(message: string, context: string) {
+        const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+        this.logger.debug(message, { context, time });
     }
 
-    verbose(message: string, ...args: any[]) {
-        this.logger.verbose(`${message} | ${args.join(' | ')}`);
+    verbose(message: string, context: string) {
+        const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+        this.logger.verbose(message, { context, time });
     }
 
-    fatal(message: string, ...args: any[]) {
-        this.logger.error(`[FATAL] ${message} | ${args.join(' | ')}`);
+    fatal(message: string, context: string) {
+        const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+        this.logger.error(`[FATAL] ${message}`, { context, time });
     }
 }
